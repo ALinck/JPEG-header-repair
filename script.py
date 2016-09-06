4b548d8fd100eb1763dd504f8fe60494031f5586
Alex Linck <linck.alex@gmail.com>
2016-09-06 01:38:39 -0400

    Update script.py
    Added dialog boxes

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 def header_repair():
     import mmap
     import os
+    import ctypes
     from tkinter.filedialog import askdirectory
     filename = askdirectory()
     headers = {"jpg": b'\xff\xd8\xff'}
@@ -18,10 +19,47 @@ def header_repair():
                     f.close()
                 else:
                     f.close()
-    # tracks files that were repaired
-    if len(rep_files) > 0:
-        i = 0
-        print(str(len(rep_files)) + " files have been repaired.\n")
-        while i < len(rep_files):
-            print(rep_files[i])
-            i += 1
+
+    def results():
+        _user32 = ctypes.WinDLL('user32', use_last_error=True)
+        _MessageBoxW = _user32.MessageBoxW
+        IDYES = 6
+        IDNO = 7
+        IDOK = 0
+        if len(rep_files) > 0:
+            def MessageBoxW(title, text, style):
+                result = _MessageBoxW(0, text, title, style)
+                if not result:
+                    raise ctypes.WinError(ctypes.get_last_error())
+                return result
+
+            def main():
+                try:
+                    result = MessageBoxW("Results", str(len(rep_files)) +
+                    " files have been repaired.\nWould you like to view the file names?", 4)
+                    if result == IDYES:
+                        i = 0
+                        while i < len(rep_files):
+                            print(rep_files[i])
+                            i += 1
+                except WindowsError as win_err:
+                    print("An error occurred:\n{}".format(win_err))
+
+            main()
+
+        else:
+            def MessageBoxW(title, text, style):
+                result = _MessageBoxW(0, text, title, style)
+                if not result:
+                    raise ctypes.WinError(ctypes.get_last_error())
+                return result
+
+            def main():
+                try:
+                    MessageBoxW("Results", "No bad headers were found", 0)
+                except WindowsError as win_err:
+                    print("An error occurred:\n{}".format(win_err))
+
+            main()
+
+    results()
