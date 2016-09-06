dbab43f274600c299f6341741bb39892b2949269
Alex Linck <linck.alex@gmail.com>
2016-09-05 23:24:06 -0400

    Update script.py
    - changed headers variable to dictionary for future addition of more jpeg headers
    - added list to track changes made

--- script.py
+++ script.py
@@ -1,17 +1,27 @@
-import mmap
-import os
-from tkinter.filedialog import askdirectory
-
-filename = askdirectory()
-for subdir, dirs, files in os.walk(filename):
-    for file in files:
-        filepath = subdir + os.sep + file
-
-        if not (not filepath.endswith(".jpg") and not filepath.endswith(".JPG")):
-            header = b'\xff\xd8\xff'
-            f = open(filepath, 'r+b')
-            s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_WRITE)
-            loc = s.find(header)
-            if loc != -1:
-                f.write(s[loc:])
-                f.close()
+def header_repair():
+    import mmap
+    import os
+    from tkinter.filedialog import askdirectory
+    filename = askdirectory()
+    headers = {"jpg": b'\xff\xd8\xff'}
+    rep_files = []
+    for subdir, dirs, files in os.walk(filename):
+        for file in files:
+            filepath = subdir + os.sep + file
+            if not (not filepath.endswith(".jpg") and not filepath.endswith(".JPG")):
+                f = open(filepath, 'r+b')
+                s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_WRITE)
+                loc = s.find(headers["jpg"])
+                if loc != -1 and loc != 0:
+                    rep_files.append(filepath)
+                    f.write(s[loc:])
+                    f.close()
+                else:
+                    f.close()
+    # tracks files that were repaired
+    if len(rep_files) > 0:
+        i = 0
+        print(str(len(rep_files)) + " files have been repaired.\n")
+        while i < len(rep_files):
+            print(rep_files[i])
+            i += 1
